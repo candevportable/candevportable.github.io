71273ccc00b7c833e95be93041bcab6a57b0e0bb
pdaniel37 <pedrodaniel.unb@gmail.com>
2023-04-01 13:06:22 -0300

    (feat) Lançamento da versão alpha-01

---
+++ lib/screens/how_it_works.dart
@@ -0,0 +1,372 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import 'contents/how_it_works_content.dart';
+
+class HowItWorks extends StatelessWidget {
+  const HowItWorks({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: const BackButton(
+          color: Color(0XFF0A0024),
+        ),
+        backgroundColor: Colors.white,
+        elevation: 0,
+        centerTitle: true,
+        title: const Text(
+          "CanDev",
+          style: TextStyle(
+              color: Color(0XFF0A0024),
+              fontFamily: "Neuropolitical Rg",
+              fontSize: 26),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 20),
+          child: Column(
+            children: [
+              Text(
+                "Como Funciona",
+                textAlign: TextAlign.center,
+                style: GoogleFonts.roboto(
+                  textStyle: const TextStyle(
+                    fontWeight: FontWeight.w700,
+                    fontSize: 30,
+                    height: 1.5,
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Expanded(
+                    child: Text.rich(
+                      TextSpan(
+                        text: "1 - Contato:",
+                        style: const TextStyle(
+                          fontWeight: FontWeight.w600,
+                          fontSize: 22,
+                        ),
+                        children: <InlineSpan>[
+                          TextSpan(
+                            text: how1,
+                            style: GoogleFonts.roboto(
+                              textStyle: const TextStyle(
+                                fontWeight: FontWeight.w400,
+                                fontSize: 22,
+                                height: 1.3,
+                                overflow: TextOverflow.clip,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Expanded(
+                    child: Text.rich(
+                      TextSpan(
+                        text: "2 - Requisitos:",
+                        style: const TextStyle(
+                          fontWeight: FontWeight.w600,
+                          fontSize: 22,
+                        ),
+                        children: <InlineSpan>[
+                          TextSpan(
+                            text: how2,
+                            style: GoogleFonts.roboto(
+                              textStyle: const TextStyle(
+                                fontWeight: FontWeight.w400,
+                                fontSize: 22,
+                                height: 1.3,
+                                overflow: TextOverflow.clip,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Expanded(
+                    child: Text.rich(
+                      TextSpan(
+                        text: "3 - Design:",
+                        style: const TextStyle(
+                          fontWeight: FontWeight.w600,
+                          fontSize: 22,
+                        ),
+                        children: <InlineSpan>[
+                          TextSpan(
+                            text: how3,
+                            style: GoogleFonts.roboto(
+                              textStyle: const TextStyle(
+                                fontWeight: FontWeight.w400,
+                                fontSize: 22,
+                                height: 1.3,
+                                overflow: TextOverflow.clip,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Expanded(
+                    child: Text.rich(
+                      TextSpan(
+                        text: "4 - Desenvolvimento:",
+                        style: const TextStyle(
+                          fontWeight: FontWeight.w600,
+                          fontSize: 22,
+                        ),
+                        children: <InlineSpan>[
+                          TextSpan(
+                            text: how4,
+                            style: GoogleFonts.roboto(
+                              textStyle: const TextStyle(
+                                fontWeight: FontWeight.w400,
+                                fontSize: 22,
+                                height: 1.3,
+                                overflow: TextOverflow.clip,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Expanded(
+                    child: Text.rich(
+                      TextSpan(
+                        text: "5 - Validação:",
+                        style: const TextStyle(
+                          fontWeight: FontWeight.w600,
+                          fontSize: 22,
+                        ),
+                        children: <InlineSpan>[
+                          TextSpan(
+                            text: how5,
+                            style: GoogleFonts.roboto(
+                              textStyle: const TextStyle(
+                                fontWeight: FontWeight.w400,
+                                fontSize: 22,
+                                height: 1.3,
+                                overflow: TextOverflow.clip,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Expanded(
+                    child: Text.rich(
+                      TextSpan(
+                        text: "6 - Lançamento:",
+                        style: const TextStyle(
+                          fontWeight: FontWeight.w600,
+                          fontSize: 22,
+                        ),
+                        children: <InlineSpan>[
+                          TextSpan(
+                            text: how6,
+                            style: GoogleFonts.roboto(
+                              textStyle: const TextStyle(
+                                fontWeight: FontWeight.w400,
+                                fontSize: 22,
+                                height: 1.3,
+                                overflow: TextOverflow.clip,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Expanded(
+                    child: Text.rich(
+                      TextSpan(
+                        text: "7 - Suporte:",
+                        style: const TextStyle(
+                          fontWeight: FontWeight.w600,
+                          fontSize: 22,
+                        ),
+                        children: <InlineSpan>[
+                          TextSpan(
+                            text: how7,
+                            style: GoogleFonts.roboto(
+                              textStyle: const TextStyle(
+                                fontWeight: FontWeight.w400,
+                                fontSize: 22,
+                                height: 1.3,
+                                overflow: TextOverflow.clip,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Expanded(
+                    child: Text.rich(
+                      TextSpan(
+                        text: "8 - Manutenção:",
+                        style: const TextStyle(
+                          fontWeight: FontWeight.w600,
+                          fontSize: 22,
+                        ),
+                        children: <InlineSpan>[
+                          TextSpan(
+                            text: how8,
+                            style: GoogleFonts.roboto(
+                              textStyle: const TextStyle(
+                                fontWeight: FontWeight.w400,
+                                fontSize: 22,
+                                height: 1.3,
+                                overflow: TextOverflow.clip,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Expanded(
+                    child: Text.rich(
+                      TextSpan(
+                        text: "9 - Marketing e divulgação (opcional):",
+                        style: const TextStyle(
+                          fontWeight: FontWeight.w600,
+                          fontSize: 22,
+                        ),
+                        children: <InlineSpan>[
+                          TextSpan(
+                            text: how9,
+                            style: GoogleFonts.roboto(
+                              textStyle: const TextStyle(
+                                fontWeight: FontWeight.w400,
+                                fontSize: 22,
+                                height: 1.3,
+                                overflow: TextOverflow.clip,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Expanded(
+                    child: Text.rich(
+                      TextSpan(
+                        text: "10 - Análise de dados (opcional):",
+                        style: const TextStyle(
+                          fontWeight: FontWeight.w600,
+                          fontSize: 22,
+                        ),
+                        children: <InlineSpan>[
+                          TextSpan(
+                            text: how10,
+                            style: GoogleFonts.roboto(
+                              textStyle: const TextStyle(
+                                fontWeight: FontWeight.w400,
+                                fontSize: 22,
+                                height: 1.3,
+                                overflow: TextOverflow.clip,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
